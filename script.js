62d0a8c05d02bba6eecbacff1397fa24fa4be0c0
Kulveer Singh <kulveer.coder@gmail.com>
2023-10-15 18:41:54 +0530

    signin btn

---
+++ script.js
@@ -0,0 +1,8 @@
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+signinBtn.onclick = function() {
+    nameField.style.maxHeight = "0";
+}
